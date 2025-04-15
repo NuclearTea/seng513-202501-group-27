be8d0b7d15c56c2da9f60ec3ccf335482963234a
NuclearTea <muhammad.ali4>
2025-04-14 18:44:33 -0600

    wip

--- UI/src/hooks/useSayHello.ts
+++ UI/src/hooks/useSayHello.ts
@@ -1,22 +1,25 @@
 import { useState } from "react";
 
-// @ts-expect-error: CommonJS modules
-import * as proto from "../../gen/web/greeter_pb";
-// @ts-expect-error: CommonJS modules
-import * as services from "../../gen/web/greeter_grpc_web_pb";
+// // @ts-expect-error: CommonJS modules
+// import * as proto from "../../gen/web/greeter_pb";
+// // @ts-expect-error: CommonJS modules
+// import * as services from "../../gen/web/greeter_grpc_web_pb";
+//
 
+import { GreeterClient } from "../../gen/web/GreeterServiceClientPb.ts";
+import { HelloRequest } from "../../gen/web/greeter_pb";
 export function useSayHello() {
   const [response, setResponse] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  const client = new services.GreeterClient("http://localhost:8080");
+  const client = new GreeterClient("http://localhost:8080");
 
   const sayHello = (name: string) => {
     setLoading(true);
     setError(null);
 
-    const req = new proto.HelloRequest();
+    const req = new HelloRequest();
     req.setName(name);
 
     client.sayHello(req, {}, (err, res) => {
